17efb3fb1074d035873b5083727a7cb62dd5d7bf
Linca <xiaochen.23@intl.zju.edu.cn>
2024-06-03 01:18:10 +0800

    delete classcode

--- Class_Code/lec1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package Class_Code.lec1;
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println(5 + "10"); /*510*/
-        System.out.println(5 + 10); /*15*/
-    }
-}
