6d5bf7b23e5dd71b5b0678bf2b0ff1a5c1e39e71
Linca <xiaochen.23@intl.zju.edu.cn>
2024-06-04 23:27:13 +0800

    lab2 is finished

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -47,6 +47,11 @@ public class DebugExercise2 {
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
             int biggerValue = max(a[i], b[i]);
+            if (biggerValue == a[i]) {
+                biggerValue = b[i];
+            } else {
+                biggerValue = a[i];
+            }
             returnArray[i] = biggerValue;
         }
 
@@ -58,8 +63,8 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
-            i = i + 1;
+            sum += x[i];
+            i++;
         }
         return sum;
     }
