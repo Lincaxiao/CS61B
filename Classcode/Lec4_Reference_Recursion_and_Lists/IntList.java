e083722b7efa44e84149cf9c83f09b2a67e594a4
Linca <xiaochen.23@intl.zju.edu.cn>
2024-06-05 21:27:19 +0800

    Add Lec4

---
+++ Classcode/Lec4_Reference_Recursion_and_Lists/IntList.java
@@ -0,0 +1,47 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    /** Returns the ith item of this IntList. */
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return rest.get(i - 1);
+    }
+
+    /** Method to return a string representation of an IntList */
+    @Override
+    public String toString() {
+        if (rest == null) {
+            // Converts an Integer to a String!
+            return String.valueOf(first);
+        } else {
+            return first + " -> " + rest.toString();
+        }
+    }
+}
